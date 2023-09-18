d4d2bf91cfddc724434a1b0136e79cc7a2a2f73b
Vaishnavimahajan <88333713+Vaishnavimahajan@users.noreply.github.com>
2023-09-18 15:18:20 +0530

    Add files via upload

---
+++ Simple_Calculator.py
@@ -0,0 +1,31 @@
+num1 = input("Enter 1st Number: ")
+num2 = input("Enter 2nd Number: ")
+# Addition
+sum = int(num1) + int(num2)
+print("The Addition is:",sum)
+print("****************************")
+# Substraction
+sub = int (num1) - int (num2)
+print("The Substraction is:",sub)
+print("****************************")
+#Multiplication
+mul = int(num1) * int(num2)
+print("The Multiplcation is: ",mul)
+print("****************************")
+# Division
+div = int (num1) / int(num2)
+print("The Division is: ",div)
+print("****************************")
+# Module
+mod = int(num1) % int(num2)
+print("The Module is:",mod)
+print("***************************")
+# Exponentiation
+exp = int(num1) ** int(num2)
+print("The Exponentiation is: ",exp)
+print("***************************")
+# Floor division
+fl = int(num1) // int(num2)
+print("The Floor division is: ",fl)
+print("**********End**************")
+
